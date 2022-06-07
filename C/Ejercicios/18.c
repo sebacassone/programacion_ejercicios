d6739c4db641f9f510dfa6ac356a04a49fefcc11
Sebastián <sebastian.gonzalez.cass@usach.cl>
2022-06-07 02:27:42 -0400

    si

--- C/Ejercicios/18.c
+++ C/Ejercicios/18.c
@@ -5,22 +5,6 @@ typedef struct rgb{
     int *posicion;
 }pixel;
 
-pixel* crear_pixel(int numero){
-    // Se declara variable tipo estructura
-    pixel *nuevo_pixel;
-    // Se asigna espacio en memoria para la estructura
-    nuevo_pixel = (pixel *)malloc(sizeof(pixel));
-    if(numero){
-        // Se le pide los datos al usuario
-        printf("Ingrese los valores para x");
-        scanf("%f", &nuevo_pixel -> colores);
-        printf("Ingrese los valores para y");
-        scanf("%f", &nuevo_pixel -> posicion);
-    }
-    // Retorna el punto
-    return nuevo_pixel;   
-}
-
 int main(void){
     return 0;
 }
---
+++ C/Ejercicios/20.c
@@ -0,0 +1,60 @@
+/*
+Este programa tiene el propósito de identificar si
+todos los elementos de una pila son positivos.
+*/
+#include<stdio.h>
+
+typedef struct nodo{
+    int info;
+    nodo *next;
+} nodo;
+
+typedef struct stack{
+    nodo *top;
+} stack;
+
+int positivos(stack *c){
+    // Se crea una variable auxiliar
+    stack *aux;
+    // Se verifica si la pila está vacía
+    if(c -> top == NULL){
+        printf("La pila está vacía");
+    }
+    // Se crea un nodo para aux
+    aux = c;
+    // Se hace un ciclo hasta el primer elemento
+    // de abajo
+    while(aux -> top -> next != NULL){
+        // Se verifica que el elemento de arriba
+        // sea positivo
+        if(aux -> top -> info < 0){
+            return 0;
+        }
+        pop(aux);
+    }
+    // Si sale del ciclo es de tipo entero
+    return 1;
+}
+
+stack pop(stack *c){
+    stack *aux;
+    aux = c;
+
+    // Se retorna la pila con el elemento eliminado
+    return c;
+}
+
+nodo* crear_nodo(){
+    // Se declara variable nuevo
+    stack *nuevo;
+    // Se asigna memoria de manera dinamica a nuevo
+    nuevo = (nodo *)malloc(sizeof(int) + sizeof(nodo *));
+    // Se le asignan valores por defecto
+    nuevo -> top = NULL;
+    // Retorna el nuevo nodo
+    return nuevo;
+}
+
+int main(void){
+    return 0;
+}
